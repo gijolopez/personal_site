7cafe65240f0b47cfc545208be82c5372e52b2f8
gijolopez <glopez22@me.com>
2018-04-19 13:22:13 -0600

    added a case statement and four methods - self.index,self.about,self.error,self.render_view

---
+++ app/controllers/personal_site.rb
@@ -0,0 +1,28 @@
+require 'rack'
+
+class PersonalSite
+  def self.call(env)
+    case env["PATH_INFO"]
+    when '/' then index
+    when '/about' then about
+    else
+      error
+    end
+  end
+
+  def self.index
+    render_view('index.html')
+  end
+
+  def self.about
+    render_view('about.html')
+  end
+
+  def self.error
+    render_view('error.html', '404')
+  end
+
+  def self.render_view
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]# Recall, this array includes the HTTP response status
+  end
+end
